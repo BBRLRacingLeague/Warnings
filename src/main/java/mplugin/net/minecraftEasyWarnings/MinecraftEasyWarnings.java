7071fcd5dd1a2bcf069f8e9193c4c10a88fb3b93
dillsteamgamer-hash <dillsteamgamer@gmail.com>
2026-06-06 14:19:32 +0100

    DatabaseManager created on load, and passed into commands where necessary

--- src/main/java/mplugin/net/minecraftEasyWarnings/MinecraftEasyWarnings.java
+++ src/main/java/mplugin/net/minecraftEasyWarnings/MinecraftEasyWarnings.java
@@ -1,17 +1,24 @@
 package mplugin.net.minecraftEasyWarnings;
 
+import mplugin.net.minecraftEasyWarnings.resources.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
 
 public final class MinecraftEasyWarnings extends JavaPlugin {
 
+    DatabaseManager databaseManager;
+    Connection databaseConnection;
+
     @Override
     public void onEnable() {
-
-
+        databaseManager = new DatabaseManager(this);
+        databaseManager.initDatabase();
+        databaseConnection = databaseManager.getConnection();
     }
 
     @Override
     public void onDisable() {
-
+        databaseManager.closeDatabase();
     }
 }
