b07c8662da51aa95eeff216769cb19859af95690
dillsteamgamer-hash <dillsteamgamer@gmail.com>
2026-06-06 14:18:59 +0100

    Updated table to use AUTOINCRETEMENT id as a primary key

--- src/main/java/mplugin/net/minecraftEasyWarnings/resources/DatabaseManager.java
+++ src/main/java/mplugin/net/minecraftEasyWarnings/resources/DatabaseManager.java
@@ -33,8 +33,8 @@ public class DatabaseManager {
         try (Statement stmt = connection.createStatement()) {
             stmt.execute("""
                                 CREATE TABLE IF NOT EXISTS regions (
-                                    id INTEGER,
-                                    culprit UUID PRIMARY KEY,
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    culprit UUID SECONDARY KEY,
                                     issuer UUID SECONDARY KEY,
                                     reason TEXT
                                 )
