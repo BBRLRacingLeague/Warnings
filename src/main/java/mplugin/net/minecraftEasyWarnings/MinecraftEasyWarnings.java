e9bd90e537dfd927dc6abc43b952847dc030f6fd
dillsteamgamer-hash <dillsteamgamer@gmail.com>
2026-06-06 13:46:04 +0100

    Initial commit

---
+++ src/main/java/mplugin/net/minecraftEasyWarnings/MinecraftEasyWarnings.java
@@ -0,0 +1,17 @@
+package mplugin.net.minecraftEasyWarnings;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftEasyWarnings extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
