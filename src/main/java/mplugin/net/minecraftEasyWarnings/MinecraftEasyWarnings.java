b77420d46c0e3f35a752a8a59e09f73e49d1d35e
dillsteamgamer-hash <dillsteamgamer@gmail.com>
2026-06-06 15:31:29 +0100

    Set up WebhookService in onEnable

--- src/main/java/mplugin/net/minecraftEasyWarnings/MinecraftEasyWarnings.java
+++ src/main/java/mplugin/net/minecraftEasyWarnings/MinecraftEasyWarnings.java
@@ -1,9 +1,12 @@
 package mplugin.net.minecraftEasyWarnings;
 
+import mplugin.net.minecraftEasyWarnings.minecraftCommands.warn;
 import mplugin.net.minecraftEasyWarnings.resources.DatabaseManager;
+import mplugin.net.minecraftEasyWarnings.resources.WebhookService;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Connection;
+import java.util.Objects;
 
 public final class MinecraftEasyWarnings extends JavaPlugin {
 
@@ -12,9 +15,13 @@ public final class MinecraftEasyWarnings extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        //Initialise the database
         databaseManager = new DatabaseManager(this);
         databaseManager.initDatabase();
         databaseConnection = databaseManager.getConnection();
+
+        //Set up Webhook Service
+        WebhookService webhookService = new WebhookService(this);
     }
 
     @Override
